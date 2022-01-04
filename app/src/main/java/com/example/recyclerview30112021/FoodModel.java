3ee99c68869cc5b584263134a3fb283f0a982c76
phamtanphat <phatdroid94@gmail.com>
2022-01-04 19:22:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview30112021/FoodModel.java
@@ -0,0 +1,8 @@
+package com.example.recyclerview30112021;
+
+public class FoodModel {
+    private int image;
+    private String name;
+    private String address;
+
+}
---
+++ app/src/main/java/com/example/recyclerview30112021/ServiceEnum.java
@@ -0,0 +1,5 @@
+package com.example.recyclerview30112021;
+
+public enum ServiceEnum {
+    SMALL_RESTAURANT , BIRTHDAY , FAMILY , GROUP , BUFFET , RESTAURANT
+}

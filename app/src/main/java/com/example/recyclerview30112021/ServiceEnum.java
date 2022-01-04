04df96148c8a8c73f340fe7c7c0c5721749a1673
phamtanphat <phatdroid94@gmail.com>
2022-01-04 19:25:25 +0700

    enum string literal

--- app/src/main/java/com/example/recyclerview30112021/ServiceEnum.java
+++ app/src/main/java/com/example/recyclerview30112021/ServiceEnum.java
@@ -1,5 +1,21 @@
 package com.example.recyclerview30112021;
 
 public enum ServiceEnum {
-    SMALL_RESTAURANT , BIRTHDAY , FAMILY , GROUP , BUFFET , RESTAURANT
+
+    SMALL_RESTAURANT("Quán ăn nhỏ"),
+    BIRTHDAY("Sinh nhật"),
+    FAMILY("Gia đình"),
+    GROUP("Nhóm hội"),
+    BUFFET("Buffet"),
+    RESTAURANT("Nhà hàng");
+
+    private final String name;
+
+    ServiceEnum(String s) {
+        name = s;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }

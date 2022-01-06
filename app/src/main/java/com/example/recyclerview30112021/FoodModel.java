0f8c88353cd989844acf808f462938815a85b941
phamtanphat <phatdroid94@gmail.com>
2022-01-06 19:10:01 +0700

    data mock

--- app/src/main/java/com/example/recyclerview30112021/FoodModel.java
+++ app/src/main/java/com/example/recyclerview30112021/FoodModel.java
@@ -117,7 +117,16 @@ public class FoodModel {
 
     public static List<FoodModel> getMock(){
         return new ArrayList<>(Arrays.asList(
-                new FoodModel(R.drawable.image_bunbodatthanh,"Bún Bò Đất Thánh - Shop Online","221/16 Đất Thánh, P. 6, Tân Bình, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.BIRTHDAY),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 20%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),13.1f)
+                new FoodModel(R.drawable.image_bunbodatthanh,"Bún Bò Đất Thánh - Shop Online","221/16 Đất Thánh, P. 6, Tân Bình, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.BIRTHDAY),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 20%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),13.1f),
+                new FoodModel(R.drawable.image_rulesoftead,"Rules Of Tea - Trà Sữa Đế Vương - Nguyễn Văn Cừ","213D Nguyễn Văn Cừ, P. 3, Quận 5, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.FAMILY,ServiceEnum.GROUP),new Discount(DiscountSessionEnum.NIGHT,"Giảm 15%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),12.7f),
+                new FoodModel(R.drawable.image_tala,"Tá Lả - Ăn Vặt, Mì Xào, Cơm Chiên & Sinh Tố - Phan Văn Trị","497/2 Phan Văn Trị, P. 5, Gò Vấp, TP. HCM",Arrays.asList(ServiceEnum.BUFFET),new Discount(DiscountSessionEnum.NOON,"Giảm 15%"),Utils.getTime(11,30,0),Utils.getTime(17,0,0),16.8f),
+                new FoodModel(R.drawable.image_anhquan,"Anh Quán - Mì Khô Gà Quay & Hủ Tiếu Mì Sủi Cảo","80/17/138 Dương Quảng Hàm, P. 5, Gò Vấp, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.FAMILY,ServiceEnum.GROUP),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 10%"),Utils.getTime(6,0,0),Utils.getTime(20,0,0),16.3f),
+                new FoodModel(R.drawable.image_cohai,"Cô Hai - Bánh Bò & Bánh Chuối Nước Dừa","200/76 Xóm Chiếu, P. 14, Quận 4, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.BIRTHDAY),new Discount(DiscountSessionEnum.NOON,"Giảm 30%"),Utils.getTime(11,30,0),Utils.getTime(17,0,0),14f),
+                new FoodModel(R.drawable.image_kimbaptiti,"Kimbap TiTi - Món Hàn Quốc - Hoàng Sa","159/1/5 Trần Văn Đang, P. 11, Quận 3, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.FAMILY,ServiceEnum.GROUP),new Discount(DiscountSessionEnum.NOON,"Giảm 20%"),Utils.getTime(11,30,0),Utils.getTime(17,0,0),15.8f),
+                new FoodModel(R.drawable.image_anvi,"An Vi - Gà Rán & Khoai Lang Lắc - Kênh Tân Hóa","128 Kênh Tân Hóa, P. Phú Trung, Tân Phú, TP. HCM",Arrays.asList(ServiceEnum.GROUP,ServiceEnum.SMALL_RESTAURANT),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 5%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),10.1f),
+                new FoodModel(R.drawable.image_tocotoco,"TocoToco Bubble Tea - Thống Nhất","195B Thống Nhất, P. 11, Gò Vấp, TP. HCM",Arrays.asList(ServiceEnum.GROUP,ServiceEnum.SMALL_RESTAURANT),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 20%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),13.1f),
+                new FoodModel(R.drawable.image_myquang3anhem,"Mì Quảng 3 Anh Em - 23 Pasteur","23 Pasteur, P. Nguyễn Thái Bình, Quận 1, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.BIRTHDAY),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 20%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),13.1f),
+                new FoodModel(R.drawable.image_sucsongmoi,"Sức Sống Mới - Nước Ép & Sinh Tố - Lê Lợi","27 Lê Lợi, P. 4, Gò Vấp, TP. HCM",Arrays.asList(ServiceEnum.RESTAURANT,ServiceEnum.BIRTHDAY),new Discount(DiscountSessionEnum.ALL_TIME,"Giảm 20%"),Utils.getTime(7,0,0),Utils.getTime(22,0,0),13.1f)
         ));
     }
 }
